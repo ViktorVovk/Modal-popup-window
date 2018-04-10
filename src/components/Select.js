eb2852adc0ece9aa1da08f620f5c2411d58f4b6b
ViktorVovk <34575602+ViktorVovk@users.noreply.github.com>
2018-04-10 21:50:44 +0300

    Update Select.js

--- src/components/Select.js
+++ src/components/Select.js
@@ -2,51 +2,53 @@ import React, { PureComponent } from 'react';
 import SelectField from 'material-ui/SelectField';
 import MenuItem from 'material-ui/MenuItem';
 import TextField from 'material-ui/TextField'
-import Delete from 'material-ui/svg-icons/navigation/cancel';
+import DeleteIcon from 'material-ui/svg-icons/navigation/close';
 import IconButton from 'material-ui/IconButton';
-import {pink100} from 'material-ui/styles/colors';
+import {pink50, red500} from 'material-ui/styles/colors';
 
 const styleSelect = {
   width: 130,
 }
 const styleTextField = {
   width: 40,
+  marginRight: 20
 }
-const styleByttonCancel = {
-  width:40,
-  height:40,
-  backgroundColor: 'red',
+const styleDeleteIcon = {
+  width:30,
+  height:30,
+}
+const styleDeleteBytton = {
+  width:36,
+  height:36,
+  padding:0,
+  backgroundColor: pink50,
   borderRadius: '50%',
-
-}
-const styleIconCancel = {
-  width:50,
-  height:50,
-  position: 'absolute',
-  top: -5,
-  right: -5,
-}
+};
 
 class Select extends PureComponent{
   state = {
-    value: 2,
+    valueSelectField: 2,
+    valueTextField: '',
   };
-  handleChangeSelectField = (event, index, value) => {
-      this.setState({value});
-    };
 
+  handleChangeSelectField = (event, index, value) => {
+      this.setState({valueSelectField: value});
+  };
+
+  handleChangeTextField = (event) => {
+    this.setState({valueTextField: event.target.value})
+  }
     render() {
       return (
         <div className="modal__select" index={this.props.index}>
-        <SelectField value={this.state.value} onChange={this.handleChangeSelectField} style={styleSelect}  iconStyle={{fill:'rgba(0,0,0,0.7)'}} underlineStyle={{borderWidth:2, width:115}}>
+        <SelectField value={this.state.valueSelectField} onChange={this.handleChangeSelectField} style={styleSelect}  iconStyle={{fill:'rgba(0,0,0,0.7)'}} underlineStyle={{borderWidth:2, width:115}}>
           <MenuItem value={1} key={1} primaryText={`Twin`} />
           <MenuItem value={2} key={2} primaryText={`Tripple`} />
           <MenuItem value={3} key={3} primaryText={`Quadro`} />
         </SelectField>
-        <TextField type="number" multiLine={false} name="textfield" style={styleTextField} underlineStyle={{borderWidth:2}}/>
-        <IconButton onClick={this.props.deleteSelectLine} iconStyle={styleIconCancel}
-          style={styleByttonCancel} >
-          <Delete color={pink100}/>
+        <TextField type="number" multiLine={false} name="textfield" style={styleTextField} value={this.state.valueTextField} underlineStyle={{borderWidth:2}} onChange={this.handleChangeTextField}/>
+        <IconButton onClick={this.props.deleteSelectLine} style={styleDeleteBytton} iconStyle={styleDeleteIcon}>
+          <DeleteIcon color={red500}/>
         </IconButton>
         </div>
       );
